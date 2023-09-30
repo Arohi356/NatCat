fb4fb3b4a0fb1e78fbbd8bd5533cf46949101f80
Arohi356 <146508465+Arohi356@users.noreply.github.com>
2023-09-30 20:52:14 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -12,18 +12,18 @@ app.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint to handle risk index lookup
-app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
     // Your logic for the home route
-    res.send('Hello, this is the home page.');
-});
+   // res.send('Hello, this is the home page.');
+//});
 
-app.get('/lookup', (req, res) => {
+app.get('/', (req, res) => {
 //  res.send('Hello, this is the home page2.');
   const { lat, long } = req.query;
   const workbook = xlsx.readFile('public/risk-data.xlsx'); // Change the filename as needed
   const sheetName = 'Sheet1'; // Change the sheet name as needed
   const worksheet = workbook.Sheets[sheetName];
-  res.send('Hello, this is the home page3.');
+  //res.send('Hello, this is the home page3.');
   if (!worksheet) {
     res.status(500).json({ error: 'Worksheet not found' });
     return;
